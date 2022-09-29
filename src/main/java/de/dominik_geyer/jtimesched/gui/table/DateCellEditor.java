8d8ba7a1549a488a555862494843a287df996410
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2022-09-29 15:56:19 +0100

    update: format files according to google rules

--- src/main/java/de/dominik_geyer/jtimesched/gui/table/DateCellEditor.java
+++ src/main/java/de/dominik_geyer/jtimesched/gui/table/DateCellEditor.java
@@ -1,17 +1,17 @@
 /* jTimeSched - A simple and lightweight time tracking tool
  * Copyright (C) 2010-2012 Dominik D. Geyer <dominik.geyer@gmail.com>
  * See LICENSE.txt for details.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -29,51 +29,49 @@ import javax.swing.SwingConstants;
 
 import de.dominik_geyer.jtimesched.project.ProjectTime;
 
-
 @SuppressWarnings("serial")
 public class DateCellEditor extends DefaultCellEditor {
-	private JTextField tfEdit;
-	private Date oldDate;
-	
-	public DateCellEditor() {
-		super(new JTextField());
-		this.tfEdit = (JTextField) this.getComponent();
-		this.tfEdit.setHorizontalAlignment(SwingConstants.CENTER);
-	}
+  private JTextField tfEdit;
+  private Date oldDate;
 
-	@Override
-	public Object getCellEditorValue() {
-		String strDate = this.tfEdit.getText();
-		Date newDate = this.oldDate;
-		
-		if (strDate.isEmpty())
-			newDate = new Date();
-		else {
-			try {
-				newDate = ProjectTime.parseDate(strDate);
-			} catch (ParseException e) {
-				System.err.println("Invalid date-string, keeping previous value");
-			}
-		}
-		
-		return newDate;
-	}
+  public DateCellEditor() {
+    super(new JTextField());
+    this.tfEdit = (JTextField) this.getComponent();
+    this.tfEdit.setHorizontalAlignment(SwingConstants.CENTER);
+  }
 
-	@Override
-	public Component getTableCellEditorComponent(JTable table,
-			Object value, boolean isSelected, int row, int column) {
-		this.oldDate = (Date)value;
-		String strDate = ProjectTime.formatDate(this.oldDate);
-		this.tfEdit.setText(strDate);
+  @Override
+  public Object getCellEditorValue() {
+    String strDate = this.tfEdit.getText();
+    Date newDate = this.oldDate;
 
-		return this.getTfEditCopy();
-	}
+    if (strDate.isEmpty()) newDate = new Date();
+    else {
+      try {
+        newDate = ProjectTime.parseDate(strDate);
+      } catch (ParseException e) {
+        System.err.println("Invalid date-string, keeping previous value");
+      }
+    }
 
-	public Component getTfEditCopy(){
-		// Returns a copy of the object, to not expose data. 
-		JTextField tfEditCopy = (JTextField) this.getComponent();
-		tfEditCopy.setHorizontalAlignment(SwingConstants.CENTER);
-		tfEditCopy.setText(this.tfEdit.getText());
-		return tfEditCopy;
-	}
+    return newDate;
+  }
+
+  @Override
+  public Component getTableCellEditorComponent(
+      JTable table, Object value, boolean isSelected, int row, int column) {
+    this.oldDate = (Date) value;
+    String strDate = ProjectTime.formatDate(this.oldDate);
+    this.tfEdit.setText(strDate);
+
+    return this.getTfEditCopy();
+  }
+
+  public Component getTfEditCopy() {
+    // Returns a copy of the object, to not expose data.
+    JTextField tfEditCopy = (JTextField) this.getComponent();
+    tfEditCopy.setHorizontalAlignment(SwingConstants.CENTER);
+    tfEditCopy.setText(this.tfEdit.getText());
+    return tfEditCopy;
+  }
 }
