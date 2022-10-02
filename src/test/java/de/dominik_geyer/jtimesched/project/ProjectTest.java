fef04ae5f4e9f4918b49eae341bfbd9f6969e7b4
Diana Freitas <diana.amfreitas@gmail.com>
2022-10-02 11:58:18 +0100

    Add beforeEach on ProjectTest

--- src/test/java/de/dominik_geyer/jtimesched/project/ProjectTest.java
+++ src/test/java/de/dominik_geyer/jtimesched/project/ProjectTest.java
@@ -1,26 +1,32 @@
 package de.dominik_geyer.jtimesched.project;
 
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mockito;
 
 public class ProjectTest {
+  private Project project;
+
+  @BeforeEach
+  void setUp() {
+    project = new Project();
+  } 
 
   // Test setRunning and getRunning first, setSecondsOverall, getSecondsOverall
   @Test
-    public void pauseRunningTest() throws ProjectException {
-        //TODO: Diana (acabar)
-        // Given
-        Project project = new Project();
-        Project projectSpy = Mockito.spy(project);
-        projectSpy.setRunning(true);
-        Mockito.when(projectSpy.getElapsedSeconds()).thenReturn(2);
-        projectSpy.setSecondsOverall(0);
-        projectSpy.pause();
-        Assert.assertEquals(projectSpy.getSecondsOverall(), 2);
-        Assert.assertEquals(projectSpy.getSecondsToday(), 2);
-        Assert.assertFalse(projectSpy.isRunning());
-    }
+  public void pauseRunningTest() throws ProjectException {
+    //TODO: Diana (acabar)
+    // Given
+    Project projectSpy = Mockito.spy(project);
+    projectSpy.setRunning(true);
+    Mockito.when(projectSpy.getElapsedSeconds()).thenReturn(2);
+    projectSpy.setSecondsOverall(0);
+    projectSpy.pause();
+    Assert.assertEquals(projectSpy.getSecondsOverall(), 2);
+    Assert.assertEquals(projectSpy.getSecondsToday(), 2);
+    Assert.assertFalse(projectSpy.isRunning());
+  }
 
   @Test
   public void pauseNotRunningTest() {
