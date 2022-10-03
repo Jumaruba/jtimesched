df4aeb8d4706bc031af9fa79c5d726c5e855586e
Diana Freitas <diana.amfreitas@gmail.com>
2022-10-03 20:38:58 +0100

    Add Given, When, Then comments to unit tests

--- src/test/java/de/dominik_geyer/jtimesched/project/ProjectTimeTest.java
+++ src/test/java/de/dominik_geyer/jtimesched/project/ProjectTimeTest.java
@@ -15,7 +15,14 @@ public class ProjectTimeTest {
   @Test
   public void parseZeroTimeTest() {
     try {
-      assertEquals(ProjectTime.parseSeconds("0:00:00"), 0);
+      // Given 
+      String strTime = "0:00:00";
+
+      // When
+      int result = ProjectTime.parseSeconds(strTime);
+
+      // Then
+      assertEquals(0, result);
     } catch (ParseException e) {
       fail("Should not have thrown any exception");
     }
@@ -24,7 +31,14 @@ public class ProjectTimeTest {
   @Test
   public void parseSecondsTest() {
     try {
-      assertEquals(ProjectTime.parseSeconds("0:00:05"), 5);
+      // Given 
+      String strTime = "0:00:05";
+
+      // When
+      int result = ProjectTime.parseSeconds(strTime);
+
+      // Then
+      assertEquals(5, result);
     } catch (ParseException e) {
       fail("Should not have thrown any exception");
     }
@@ -33,7 +47,14 @@ public class ProjectTimeTest {
   @Test
   public void parseMinutesTest() {
     try {
-      assertEquals(ProjectTime.parseSeconds("0:15:48"), 948);
+      // Given 
+      String strTime = "0:15:48";
+
+      // When
+      int result = ProjectTime.parseSeconds(strTime);
+
+      // Then
+      assertEquals(948, result);
     } catch (ParseException e) {
       fail("Should not have thrown any exception");
     }
@@ -42,7 +63,14 @@ public class ProjectTimeTest {
   @Test
   public void parseHoursTest() {
     try {
-      assertEquals(ProjectTime.parseSeconds("6:15:48"), 22548);
+      // Given 
+      String strTime = "6:15:48";
+
+      // When
+      int result = ProjectTime.parseSeconds(strTime);
+
+      // Then
+      assertEquals(22548, result);
     } catch (ParseException e) {
       fail("Should not have thrown any exception");
     }
@@ -51,7 +79,14 @@ public class ProjectTimeTest {
   @Test
   public void parseDaysTest() {
     try {
-      assertEquals(ProjectTime.parseSeconds("36:15:48"), 130548);
+      // Given 
+      String strTime = "36:15:48";
+
+      // When
+      int result = ProjectTime.parseSeconds(strTime);
+
+      // Then
+      assertEquals(130548, result);
     } catch (ParseException e) {
       fail("Should not have thrown any exception");
     }
