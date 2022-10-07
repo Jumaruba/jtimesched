daaf14b6343ac76c4e477c07eba0ac98c7ead8bf
Diana Freitas <diana.amfreitas@gmail.com>
2022-10-07 08:21:42 +0100

    Updated test to parametrized and updated report

--- src/test/java/de/dominik_geyer/jtimesched/project/ProjectTimeTest.java
+++ src/test/java/de/dominik_geyer/jtimesched/project/ProjectTimeTest.java
@@ -6,92 +6,41 @@ import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class ProjectTimeTest {
-  /**
-   * Test for valid time and invalid?
-   */
   @ParameterizedTest
   @MethodSource("parseSecondsParams")
   public void parseSecondsTest(int s) {
-    // TODO: Diana
   }
 
   public Stream<Integer> parseSecondsParams() {
     return Stream.of(1);
   }
 
-  @Test
-  public void formatZeroTimeTest() {
-    // Given
-    int seconds = 0;
+  
+  @ParameterizedTest
+  @MethodSource("genFormatSeconds")
+  public void formatSecondsTest(int s, String expected) {
+    // Given 'seconds'
 
     // When
-    String result = ProjectTime.formatSeconds(seconds);
+    String result = ProjectTime.formatSeconds(s);
 
     // Then
-    assertEquals("0:00:00", result);
+    assertEquals(expected, result);
   }
 
-  @Test
-  public void formatSecondsTest() {
-    // Given
-    int seconds = 5;
-
-    // When
-    String result = ProjectTime.formatSeconds(seconds);
-
-    // Then
-    assertEquals("0:00:05", result);
-  }
-
-  @Test
-  public void formatMinutesTest() {
-    // Given
-    int seconds = 65;
-
-    // When
-    String result = ProjectTime.formatSeconds(seconds);
-
-    // Then
-    assertEquals("0:01:05", result);
-  }
-
-  @Test
-  public void formatHoursTest() {
-    // Given
-    int seconds = 4215;
-
-    // When
-    String result = ProjectTime.formatSeconds(seconds);
-
-    // Then
-    assertEquals("1:10:15", result);
-  }
-
-  @Test
-  public void formatDaysTest() {
-    // Given
-    int seconds = 90065;
-
-    // When
-    String result = ProjectTime.formatSeconds(seconds);
-
-    // Then
-    assertEquals("25:01:05", result);
-  }
-
-  @Test
-  public void formatNegativeTest() {
-    // Given
-    int seconds = -1;
-
-    // When
-    String result = ProjectTime.formatSeconds(seconds);
-
-    // Then
-    assertEquals("0:00:00", result);
+  public static Stream<Arguments> genFormatSeconds() {
+    return Stream.of(
+      Arguments.of(0, "0:00:00"),
+      Arguments.of(5,"0:00:05"),
+      Arguments.of(65,"0:01:05"),
+      Arguments.of(4215,"1:10:15"),
+      Arguments.of(90065,"25:01:05"),
+      Arguments.of(-1,"0:00:00")
+    );
   }
 
 }
